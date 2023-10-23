ee783975a21254dd7d4ebe2de2277656e4ce79db
Imen-Frigui <imen.frigui@esprit.tn>
2023-10-23 15:08:18 +0100

    Intiliaze

---
+++ src/main/java/tn/esprit/gestionski/entities/Moniteur.java
@@ -0,0 +1,21 @@
+package tn.esprit.gestionski.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class Moniteur implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long numMoniteur;
+    private String nomM;
+    private String prenomM;
+    @Temporal(TemporalType.DATE)
+    private Date dateRecu;
+}
